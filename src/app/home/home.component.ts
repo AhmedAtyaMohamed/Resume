c3c343611be4b2211135ca7ac117165bb3b27da5
ahmed attya <AHMEDATTYAMOHAMED@OUTLOOK.COM>
2024-10-20 06:48:43 +0300

    new push

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,7 @@ import { Component } from '@angular/core';
 })
 
 export class HomeComponent {
-LogToConsole(){
-  console.log("this log From Home componant ...");
-}
+constructor(private titleservice: Title ){
+titleservice.setTitle('ahmed attya . home');
+}    
 }
